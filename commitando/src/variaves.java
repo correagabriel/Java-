297d7970e2105af2328d6dfc8e90172787c1f732
correagabriel <gabriel.silva2270@etec.sp.gov.br>
2022-08-16 16:15:49 -0300

    dori

--- commitando/src/variaves.java
+++ commitando/src/variaves.java
@@ -1,13 +1,13 @@
 public class variaves {
     public static void main(String[] args) {
         byte idade = 123;
-            System.out.println(idade);
+            System.out.println("Sua idade = " + idade);
 
         short ano = 2022;
-            System.out.println(ano);
+            System.out.println("Ano = " + ano);
 
         int cep = 2107033;
-            System.out.println(cep);
+            System.out.println("CEP = " + cep);
                                         //se comecar com 0 tanto o long como o int altere-o para string para não dar conflito
         long cpf = 123675904909L;
             System.out.println("Seu CPF: " + cpf);
@@ -16,7 +16,7 @@ public class variaves {
             System.out.println(pi);         //tanto co long como o float você deverá colocar o F ou o L no final do numero  
 
         double salario = 1900.22;
-            System.out.println(salario);
+            System.out.println("Seu salario = " + salario);
 
     } 
 }
