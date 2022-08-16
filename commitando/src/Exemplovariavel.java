4e6ebc4cce2f0151449a25f85eafdf5395dfa5dd
correagabriel <gabriel.silva2270@etec.sp.gov.br>
2022-08-16 16:22:29 -0300

    uno

---
+++ commitando/src/Exemplovariavel.java
@@ -0,0 +1,9 @@
+public class Exemplovariavel {
+    public static void main(String[] args) {
+        int numero = 10;
+
+
+        numero = 10;
+            System.out.println("Numero = " + numero);
+    }
+}
