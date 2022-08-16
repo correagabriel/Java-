b1a45aba2f132e1e4145faafcf14a091d12b3dd0
correagabriel <gabriel.silva2270@etec.sp.gov.br>
2022-08-16 16:51:12 -0300

    k

--- commitando/src/VariavelConstante.java
+++ commitando/src/VariavelConstante.java
@@ -2,6 +2,7 @@ public class VariavelConstante {
     public static void main(String[] args) {
         final double pi = 3.14;
 
-        pi = 3.15; // erro na compilação 
+        System.out.println("Valor de PI = " + pi);
     }
 }
+             
