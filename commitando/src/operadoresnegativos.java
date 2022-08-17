75e1763cdca70795d331797ba939e7c5d5342ded
correagabriel <gabriel.silva2270@etec.sp.gov.br>
2022-08-17 16:29:37 -0300

    newcommit

---
+++ commitando/src/operadoresnegativos.java
@@ -0,0 +1,10 @@
+public class operadoresnegativos {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = - numero;
+        numero = numero * -1;
+
+            System.out.println(numero);
+    }
+}
