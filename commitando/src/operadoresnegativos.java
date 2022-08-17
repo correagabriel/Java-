963e6dda0c0df3998b5a3af5d7ceac5636c425f7
correagabriel <gabriel.silva2270@etec.sp.gov.br>
2022-08-17 16:38:16 -0300

    maisemenos

--- commitando/src/operadoresnegativos.java
+++ commitando/src/operadoresnegativos.java
@@ -2,13 +2,16 @@ public class operadoresnegativos {
     public static void main(String[] args) {
         int numero = 5;
         int num = 5;
+        int n1 = 5;
 
         numero = - numero;
         numero = numero * -1;
         num = ++num;
+        n1 = --n1;
 
             System.out.println(- numero);
             System.out.println(numero);
             System.out.println(num);
+            System.out.println(n1);
     }
 }
