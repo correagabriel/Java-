4d13beac374499878fec89b080ba159a3a3e7cef
correagabriel <gabriel.silva2270@etec.sp.gov.br>
2022-08-18 15:35:59 -0300

    muitos commits

---
+++ commitando/src/estruturaderepeticao.java
@@ -0,0 +1,11 @@
+public class estruturaderepeticao {
+    public static void main(String[] args) {
+        
+    int idade = 15;
+
+    while (idade < 18){
+        System.out.println(idade);
+        idade = idade + 1;
+    }
+    }
+}
---
+++ commitando/src/estruturaderepeticaoFOR.java
@@ -0,0 +1,14 @@
+public class estruturaderepeticaoFOR {
+    public static void main(String[] args) {
+    /*
+        for (inicialização;condição;incremento){
+        codigo;
+        }
+    */
+    
+        for(int i = 0; i < 10; i = i + 1 ){
+            System.out.println(" Hello ");
+        } 
+    }
+    
+}
